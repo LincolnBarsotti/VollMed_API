c0c23fcf3bf7d657e1bc90bfa5f1757e9b8effb8
Lincoln Barsotti <107706237+LincolnBarsotti@users.noreply.github.com>
2024-02-29 00:07:34 -0300

    pronto

--- src/main/java/med/voll/api/infra/security/TokenService.java
+++ src/main/java/med/voll/api/infra/security/TokenService.java
@@ -3,6 +3,7 @@ package med.voll.api.infra.security;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import med.voll.api.domain.usuario.Usuario;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -22,13 +23,28 @@ public class TokenService {
         try{
             var algoritimo = Algorithm.HMAC256(secret);
             return JWT.create()
-                    .withIssuer("API voll.med")
+                    .withIssuer("API Voll.med")
                     .withSubject(usuario.getLogin())
                     .withExpiresAt(dataExpiracao())
                     .sign(algoritimo);
         }catch (JWTCreationException exception){
             throw new RuntimeException("erro ao gerar token jwt", exception);
         }
+    }
+
+    public String getSubject(String tokenJWT){
+
+        try {
+            var algoritimo = Algorithm.HMAC256(secret);
+            return JWT.require(algoritimo)
+                    .withIssuer("API Voll.med")
+                    .build()
+                    .verify(tokenJWT)
+                    .getSubject();
+        } catch (JWTVerificationException exception){
+            throw new RuntimeException("JWT inválido ou expirado");
+        }
+
     }
 
     private Instant dataExpiracao() {
